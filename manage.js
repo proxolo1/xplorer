d1191af49165562f04a31cbc4f30f56e128e095f
annsneha <113496457+annsneha@users.noreply.github.com>
2022-09-14 18:29:43 +0530

    qasfdsdafasdf
    wrdsadf

--- manage.js
+++ manage.js
@@ -1,99 +1,31 @@
 let employeeData;
 
-$(document).ready(function () {
-  $('[data-toggle="tooltip"]').tooltip();
-  var actions = $("table td:last-child").html();
-  // Append table with add row form on add new button click
-  $(".add-new").click(function () {
-    $(this).attr("disabled", "disabled");
-    var index = $("table tbody tr:last-child").index();
-    var row =
-      "<tr>" +
-      '<td><input type="text" class="form-control" name="name" id="name"></td>' +
-      '<td><input type="text" class="form-control" name="department" id="department"></td>' +
-      '<td><input type="text" class="form-control" name="phone" id="phone"></td>' +
-      "<td>" +
-      actions +
-      "</td>" +
-      "</tr>";
-    $("table").append(row);
-    $("table tbody tr")
-      .eq(index + 1)
-      .find(".add, .edit")
-      .toggle();
-    $('[data-toggle="tooltip"]').tooltip();
-  });
-
-  // Add row on add button click
-  $(document).on("click", ".add", function () {
-    var empty = false;
-
-    var input = $(this).parents("tr").find('input[type="text"]');
-
-    input.each(function () {
-      if (!$(this).val()) {
-        $(this).addClass("error");
-        empty = true;
-      } else {
-        // console.log($(this));
-        // console.log(employeeData)
-
-      //  const xhr=new XMLHttpRequest();
-      //  xhr.open("POST","employee.json");
-      //  xhr.setRequestHeader('content-type','application/json;charset=UTF-8');
-      //  xhr.send("jay");
-      //  xhr.onload=function(){
-      //    console.log(this)
-      //  }
-        console.log($(this).val())
-        
-        $(this).removeClass("error");
-      }
-    });
-    $(this).parents("tr").find(".error").first().focus();
-    if (!empty) {
-      input.each(function () {
-        $(this).parent("td").html($(this).val());
-      });
-      $(this).parents("tr").find(".add, .edit").toggle();
-      $(".add-new").removeAttr("disabled");
-    }
-  });
-  // Edit row on edit button click
-  $(document).on("click", ".edit", function () {
-    $(this)
-      .parents("tr")
-      .find("td:not(:last-child)")
-      .each(function () {
-        $(this).html(
-          '<input type="text" class="form-control" value="' +
-            $(this).text() +
-            '">'
-        );
-      });
-    $(this).parents("tr").find(".add, .edit").toggle();
-    $(".add-new").attr("disabled", "disabled");
-  });
-  // Delete row on delete button click
-  $(document).on("click", ".delete", function () {
-    $(this).parents("tr").remove();
-    $(".add-new").removeAttr("disabled");
-  });
-});
-
 function loadData() {
+  let tableBody = document.querySelector("tbody");
   const xhr = new XMLHttpRequest();
   xhr.onload = function () {
     // console.log(this.responseText);
     employeeData = JSON.parse(this.responseText);
     employeeData.forEach((employee) => {
-      $("table tbody")
-        .append(`<tr><td>${employee.name}</td><td>${employee.project}</td><td>${employee.phone}</td><td> <a class="add" title="Add" data-toggle="tooltip"><i
-		class="material-icons">&#xE03B;</i></a>
-<a class="edit" title="Edit" data-toggle="tooltip"><i
-		class="material-icons">&#xE254;</i></a>
-<a class="delete" title="Delete" data-toggle="tooltip"><i
-		class="material-icons">&#xE872;</i></a></td></tr>`);
+      let row = document.createElement("tr");
+      let employeeName = document.createElement("td");
+      let employeeProject = document.createElement("td");
+      let employeePhone = document.createElement("td");
+      let action = document.createElement("td");
+      employeeName.innerHTML = `${employee.name}`;
+      employeeProject.innerHTML = `${employee.project}`;
+      employeePhone.innerHTML = `${employee.phone}`;
+      action.innerHTML = `<td> <a class="add" title="Add" data-toggle="tooltip"><i
+      class="material-icons">&#xE03B;</i></a>
+  <a class="edit" title="Edit" data-toggle="tooltip"><i
+      class="material-icons">&#xE254;</i></a>
+  <a class="delete" title="Delete" data-toggle="tooltip"><i
+      class="material-icons">&#xE872;</i></a></td>`;
+      row.append(employeeName);
+      row.append(employeeProject);
+      row.append(employeePhone);
+      row.append(action);
+      tableBody.append(row);
     });
   };
   xhr.open("GET", "./employee.json");
@@ -101,3 +33,24 @@ function loadData() {
 }
 
 loadData();
+function addEmployee() {
+  let employee={
+    name:"",
+    project:"",
+    phone:""
+  }
+  let inputs = document.querySelectorAll("input");
+  var project = document.querySelector("#project");
+  var option = project.options[project.selectedIndex];
+  const xhr=new XMLHttpRequest();
+  xhr.open();
+  xhr.send();
+  inputs.forEach((data) => {
+    if (!data.value) {
+      alert("value should not be empty");
+    } else {
+      
+      console.log(data.value, option.value);
+    }
+  });
+}
